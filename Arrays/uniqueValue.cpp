b45d90819d56068f51519b51373bee4ae8551b70
Ashwani-yadav01 <yadavashwaniyadav273209@gmail.com>
2025-08-06 21:53:41 +0530

    uniques value problem brute force

---
+++ Arrays/uniqueValue.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a[] = {1, 1, 2, 2, 5, 3, 4,};
+    int c[8];
+    int k = 0;
+    int s = 0;
+    for (int i = 0; i < 7; i++)
+    {
+        for (int j = 0; j < 7; j++)
+        {
+            if (a[i] == a[j])
+            {
+                s++;
+            }
+        }
+        if (s == 1)
+        {
+            c[k] = a[i];
+            k++;
+        }
+        s=0;
+    }
+
+    for (int i = 0; i < k; i++)
+    {
+        cout << c[i] << " ";
+    }
+
+    return 0;
+}
