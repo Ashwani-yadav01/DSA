1670d51030af14a768d4557284aec794624418be
Ashwani-yadav01 <yadavashwaniyadav273209@gmail.com>
2025-11-28 20:39:35 +0530

    getting on track

--- ForExamSubmission/StackUsingArray.c
+++ ForExamSubmission/StackUsingArray.c
@@ -4,36 +4,43 @@
 int stack[SIZE];
 int top = -1;
 
-void push(int value) {
+void push(int value)
+{
     if (top == SIZE - 1)
         printf("Stack Overflow\n");
-    else {
+    else
+    {
         top++;
         stack[top] = value;
         printf("%d pushed to stack\n", value);
     }
 }
 
-void pop() {
+void pop()
+{
     if (top == -1)
         printf("Stack Underflow\n");
-    else {
+    else
+    {
         printf("%d popped from stack\n", stack[top]);
         top--;
     }
 }
 
-void peek() {
+void peek()
+{
     if (top == -1)
         printf("Stack is empty\n");
     else
         printf("Top element is %d\n", stack[top]);
 }
 
-void display() {
+void display()
+{
     if (top == -1)
         printf("Stack is empty\n");
-    else {
+    else
+    {
         printf("Stack elements: ");
         for (int i = top; i >= 0; i--)
             printf("%d ", stack[i]);
@@ -41,15 +48,18 @@ void display() {
     }
 }
 
-int main() {
+int main()
+{
     push(10);
     push(20);
     push(30);
+
     display();
     peek();
+
     pop();
     peek();
     display();
+
     return 0;
 }
-
